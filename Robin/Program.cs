c294e44d8b99087775c5c10af44def49b5f172fc
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2020-04-26 11:18:31 +0300

    Initial commit. Scaffolding service based architecture with docker-compose.

---
+++ Robin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Robin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
